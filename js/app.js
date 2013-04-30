1f5a6123aa86153f9f17c901acc3caf2232d9b9b
Endre Karlson <endre.karlson@gmail.com>
2013-04-30 23:52:24 +0200

    Add Doc & communicate pages + fixes

--- js/app.js
+++ js/app.js
@@ -4,5 +4,6 @@ angular.module('billingstack', []).
     config(['$routeProvider', function($routeProvider) {
     $routeProvider.
         when('/', {templateUrl: 'partials/index.html'}).
-        when('/communicate', {templateUrl: 'partials/communicate.html'})
+        when('/communicate', {templateUrl: 'partials/communicate.html'}).
+        when('/documentation', {templateUrl: 'partials/documentation.html'})
     }])
